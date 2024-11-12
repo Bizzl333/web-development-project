66cd783343f46b008effe93f44a14f0cf02fbbb6
Israel Iyoha <iyohaisrael@gmail.com>
2024-11-12 13:34:03 -0500

    Milestone 2: Node.js

--- app.js
+++ app.js
@@ -2,33 +2,37 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({ extended: true })); // Middleware to parse form data
+// middleware used to parse the data from the form
+app.use(express.urlencoded({ extended: true }));
 
-// Serve the HTML form on GET request to root
+// serve the index, contact and about .html files when the user navigates to it
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-// Serve the contact form on GET request to /contact
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/about.html');
+});
+
 app.get('/contact', (req, res) => {
     res.sendFile(__dirname + '/contact.html');
 });
 
-// Handle form submission on POST request to /submit-form
+// form submission on  the POST request and retrieve the values entered by user
 app.post('/submit-form', (req, res) => {
-    const name = req.body.name; // Retrieve 'name' field from form data
-    const email = req.body.email; // Retrieve 'email' field from form data
-    const message = req.body.message; // Retrieve 'message' field from form data
+    const name = req.body.name; 
+    const email = req.body.email; 
+    const message = req.body.message;
 
-    // Check if name is provided
-    if (!name) {
-        res.send('Please enter a valid username.');
+    // Check if all the fields are filled correctly and display the appropriate message
+    if (!name || !email || !message) {
+        res.send('Error: Please enter your name, email, and message.');
     } else {
-        res.send(`Thank you, ${name}. We have received your message.`);
+        res.send(`Thank you, ${name}, we have received your message. We will reach out to you at ${email} soon.`);
     }
 });
 
-// Start the server
+// Starts the server and listens on port 3000
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
